5c2944f48534284f095c8f2f5b0870b30e5d899c
tenklers-games <tenk.alihan@gmail.com>
2026-01-22 16:58:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-  alert("JS ÇALIŞIYOR");
-});
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "red";
+ctx.fillRect(100, 150, 100, 100);
