7533bb4a9259615855fb8e58b9f02b28c30b83d3
tenklers-games <tenk.alihan@gmail.com>
2026-01-22 17:04:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(100, 150, 100, 100);
+let x = 130;
+let y = 300;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(x, y, 40, 40);
+
+  requestAnimationFrame(draw);
+}
+
+draw();
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") x -= 10;
+  if (e.key === "ArrowRight") x += 10;
+});
