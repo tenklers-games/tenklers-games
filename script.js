3755363df50ef1c59849a5471db5fb748c080495
tenklers-games <tenk.alihan@gmail.com>
2026-01-22 16:53:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-let score = 0;
-let timer;
-
-function openGame() {
-  document.getElementById("gameArea").classList.remove("hidden");
-  score = 0;
-  document.getElementById("score").innerText = score;
-
-  timer = setInterval(() => {
-    score++;
-    document.getElementById("score").innerText = score;
-  }, 1000);
-}
-
-function closeGame() {
-  document.getElementById("gameArea").classList.add("hidden");
-  clearInterval(timer);
-}
-window.onload = () => {
-  startGame();
-};
+document.addEventListener("DOMContentLoaded", () => {
+  alert("JS ÇALIŞIYOR");
+});
